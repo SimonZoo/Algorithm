01fe1d63539f1ca2772e0bdc3aa47d6d51ab29c9
SimonZoo <simonzoo@yeah.net>
2017-11-30 17:58:44 +0800

    Add 1011.c

---
+++ PAT/Advanced/1011.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void) {
+    char flag[4] = {'W', 'T', 'L'};
+    double number[3][3];
+    int max[3];
+    int max_pos = 0;
+    double sum = 1.0;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            scanf("%lf", &number[i][j]);
+            if (number[i][j] > number[i][max_pos])
+                max_pos = j;
+        }
+        max[i] = max_pos;
+    }
+    for (int i = 0; i < 3; i++) {
+        printf("%c ", flag[max[i]]);
+        sum = sum * number[i][max[i]];
+    }
+    sum = (sum * 0.65 - 1) * 2;
+    sum = ((int)(sum*100.0+0.51)) / 100.0;
+    printf("%.2lf", sum);
+}
